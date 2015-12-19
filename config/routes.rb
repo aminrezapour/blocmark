86526d0f27186233ad78e841b387a921bbc3a8a9
Amin Rezapour <aminrezapour@gmail.com>
2015-12-19 12:47:57 -0800

    Add user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
+
+  resources :users, only: [:show]
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
