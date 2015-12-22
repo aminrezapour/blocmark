5760f718a82f23c015bbf8f6c126836f8645e372
Amin Rezapour <aminrezapour@gmail.com>
2015-12-22 14:34:55 -0800

    Last changes with Jose

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,8 @@
-class RegistrationsController < Devise::RegistrationsController
-
-  protected
-
-    def after_update_path_for(resource)
-      user_path(resource)
-    end
-end
+# class RegistrationsController < Devise::RegistrationsController
+#
+#   protected
+#
+#     def after_update_path_for(resource)
+#       user_path(resource)
+#     end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users#, :controllers => { :registrations => :registrations }
 
   resources :users, only: [:show] do
     resources :topics, only: [:index, :show, :new, :edit, :create, :update, :destroy]
